76763f201be4984df26f4477585ac00dbfb2d534
hopark <pjh4421@gmail.com>
2025-10-01 11:02:21 +0900

    2025.10.01 comit 안된거

---
+++ springMVC/src/main/java/co/spring/mvc/Common/web/PublicController.java
@@ -0,0 +1,41 @@
+package co.spring.mvc.Common.web;
+
+import java.util.Map;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.spring.mvc.user.service.UserService;
+import co.spring.mvc.user.service.UserVO;
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/public/*")
+public class PublicController {
+	
+	private final UserService userService;
+	
+	@GetMapping(value = "/getDepartmentList.do")
+	@ResponseBody
+	public Map<String, Object> getDepartmentList () throws Exception {
+		return userService.getDepartmentList();
+	}
+	
+	@PostMapping(value = "/singUp.do")
+	@ResponseBody
+	public Map<String, Object> singUp (@RequestBody UserVO userVO) throws Exception {
+		return userService.singUp(userVO);
+	}
+	
+	@PostMapping(value = "/checkId.do")
+	@ResponseBody
+	public Map<String, Object> checkId (@RequestBody UserVO userVO) throws Exception {
+		return userService.checkId(userVO);
+	}
+
+}
